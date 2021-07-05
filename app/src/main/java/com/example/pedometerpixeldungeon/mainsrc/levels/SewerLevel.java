747e4faa1009990f64b420b7bebf937f97a484b9
조용두 <sanh800@daum.net>
2021-07-05 13:35:32 +0900

    1

--- app/src/main/java/com/example/pedometerpixeldungeon/mainsrc/levels/SewerLevel.java
+++ app/src/main/java/com/example/pedometerpixeldungeon/mainsrc/levels/SewerLevel.java
@@ -1,6 +1,15 @@
 package com.example.pedometerpixeldungeon.mainsrc.levels;
 
 import com.example.pedometerpixeldungeon.mainsrc.Assets;
+import com.example.pedometerpixeldungeon.mainsrc.Dungeon;
+import com.example.pedometerpixeldungeon.mainsrc.DungeonTilemap;
+import com.example.pedometerpixeldungeon.mainsrc.scenes.GameScene;
+import com.example.pedometerpixeldungeon.noosa.Game;
+import com.example.pedometerpixeldungeon.noosa.Scene;
+import com.example.pedometerpixeldungeon.noosa.particles.Emitter;
+import com.example.pedometerpixeldungeon.noosa.particles.PixelParticle;
+import com.example.pedometerpixeldungeon.utils.ColorMath;
+import com.example.pedometerpixeldungeon.utils.PointF;
 import com.example.pedometerpixeldungeon.utils.Random;
 import com.example.pedometerpixeldungeon.utils.Rect;
 
@@ -105,5 +114,104 @@ public class SewerLevel extends RegularLevel {
     @Override
     protected void createItems() {
 
+    }
+
+    @Override
+    public void addVisuals( Scene scene ) {
+        super.addVisuals( scene );
+        addVisuals( this, scene );
+    }
+
+    public static void addVisuals( Level level, Scene scene ) {
+        for (int i=0; i < LENGTH; i++) {
+            if (level.map[i] == Terrain.WALL_DECO) {
+                scene.add( new Sink( i ) );
+            }
+        }
+    }
+
+    @Override
+    public String tileName( int tile ) {
+        switch (tile) {
+            case Terrain.WATER:
+                return "Murky water";
+            default:
+                return super.tileName( tile );
+        }
+    }
+
+    @Override
+    public String tileDesc(int tile) {
+        switch (tile) {
+            case Terrain.EMPTY_DECO:
+                return "Wet yellowish moss covers the floor.";
+            case Terrain.BOOKSHELF:
+                return "The bookshelf is packed with cheap useless books. Might it burn?";
+            default:
+                return super.tileDesc( tile );
+        }
+    }
+
+    private static class Sink extends Emitter {
+
+        private int pos;
+        private float rippleDelay = 0;
+
+        private static final Emitter.Factory factory = new Factory() {
+
+            @Override
+            public void emit( Emitter emitter, int index, float x, float y ) {
+                WaterParticle p = (WaterParticle)emitter.recycle( WaterParticle.class );
+                p.reset( x, y );
+            }
+        };
+
+        public Sink( int pos ) {
+            super();
+
+            this.pos = pos;
+
+            PointF p = DungeonTilemap.tileCenterToWorld( pos );
+            pos( p.x - 2, p.y + 1, 4, 0 );
+
+            pour( factory, 0.05f );
+        }
+
+        @Override
+        public void update() {
+            if (visible = Dungeon.visible[pos]) {
+
+                super.update();
+
+                if ((rippleDelay -= Game.elapsed) <= 0) {
+                    GameScene.ripple( pos + WIDTH ).y -= DungeonTilemap.SIZE / 2;
+                    rippleDelay = Random.Float( 0.2f, 0.3f );
+                }
+            }
+        }
+    }
+
+    public static final class WaterParticle extends PixelParticle {
+
+        public WaterParticle() {
+            super();
+
+            acc.y = 50;
+            am = 0.5f;
+
+            color( ColorMath.random( 0xb6ccc2, 0x3b6653 ) );
+            size( 2 );
+        }
+
+        public void reset( float x, float y ) {
+            revive();
+
+            this.x = x;
+            this.y = y;
+
+            speed.set( Random.Float( -2, +2 ), 0 );
+
+            left = lifespan = 0.5f;
+        }
     }
 }
